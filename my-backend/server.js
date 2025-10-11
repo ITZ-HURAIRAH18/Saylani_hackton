084110ad18282e4de3b4d332c1df0f825760df45
ITZ-HURAIRAH18 <muhammadabuhurairah558@gmail.com>
2025-10-11 17:48:52 +0500

    CORS

--- my-backend/server.js
+++ my-backend/server.js
@@ -15,17 +15,19 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 // Middleware
 // app.use(cors());
-app.use(
-  cors({
-    origin: [
-      "https://saylani-hackton-9pw4-awuzbmyao.vercel.app", // your frontend
-      "http://localhost:5173", // for local dev
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       "https://saylani-hackton-9pw4-awuzbmyao.vercel.app", // your frontend
+//       "http://localhost:5173", // for local dev
+//     ],
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//     credentials: true,
+//   })
+// );
+app.use(cors({ origin: "https://saylani-hackton-9pw4-awuzbmyao.vercel.app" }));
+// Or for development, use: app.use(cors());
 
 app.use(express.json());
 
