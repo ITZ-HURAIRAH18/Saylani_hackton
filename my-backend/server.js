faae6311d7d0286d238b54c1f891bca851ca3d2a
ITZ-HURAIRAH18 <muhammadabuhurairah558@gmail.com>
2025-10-13 19:25:08 +0500

    hello

--- my-backend/server.js
+++ my-backend/server.js
@@ -15,17 +15,17 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
-app.use(
-  cors({
-    origin: [
-      "https://saylani-hackton-9pw4-awuzbmyao.vercel.app",
-      "http://localhost:5173",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       "https://saylani-hackton-9pw4-awuzbmyao.vercel.app",
+//       "http://localhost:5173",
+//     ],
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//     credentials: true,
+//   })
+// );
 
 
 app.use(express.json());
